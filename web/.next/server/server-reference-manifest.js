07a92186481429fdb70c50a73f5a88e86c1f733a
KamoteNga <kamoteka143din@gmail.com>
2025-10-05 17:05:21 +0800

    Deploy to GitHub Pages

--- web/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"GsAX3eeUpAGSIwXCgZCBZSkO70+L1nPsZaCF6qKenqQ=\"\n}"
--- web/.next/static/chunks/3c6c1ddbe2ab7971.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/f2c9ce7e50aefada.js",
-  "static/chunks/488702c8982316ab.js",
-  "static/chunks/turbopack-488f76d035f67985.js"
-])
