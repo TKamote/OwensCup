1d28546fb3e0bd22f58599369b7130c7393ffc2a
TKamote <TKamote@users.noreply.github.com>
2025-10-06 14:40:41 +0000

    deploy: e0f82a5bde50562f0ee0dc82f5fb8c204c71bae7

---
+++ _next/static/chunks/726f7f20453f4956.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/7aa8582550b803c5.js",
+  "static/chunks/cc6443e7aa767e9a.js",
+  "static/chunks/turbopack-9510e0ef52f39f48.js"
+])
---
+++ _next/static/chunks/adfe77a685643b7e.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/5a9b3faa7b32b9b6.js",
+  "static/chunks/cc6443e7aa767e9a.js",
+  "static/chunks/turbopack-ff6b4ac52813c166.js"
+])
